a66f96ff18e5d744889375bec2491e8d8ef086ad
Anuj Tambwekar <anujstam@gmail.com>
2019-02-24 10:59:15 +0530

    Create Day6.go

---
+++ Day6.go
@@ -0,0 +1,41 @@
+package main
+import(
+   "bufio" 
+   "fmt"
+   "os"
+)
+
+
+func stsplit(s string){
+    length := len(s)
+    for i:=0;i<length;i++{
+        fmt.Print(string(s[i]))
+    }
+    for j:=1;j<length;j+=2{
+        fmt.Print(string(s[j]))
+    }
+}
+
+func main() {
+ //Enter your code here. Read input from STDIN. Print output to STDOUT
+    var looper int
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Scanln(&looper)
+    for i:=0;i<looper;i++{
+        txt,_ := reader.ReadString('\n')
+        length := len(txt)
+        if txt[length-1]=='\n'{
+            length-=1
+        }
+        for i:=0;i<length;i+=2{
+            fmt.Print(string(txt[i]))
+        }
+        fmt.Print(" ")
+        for j:=1;j<length;j+=2{
+            fmt.Print(string(txt[j]))
+        }
+        fmt.Print("\n")
+    }
+
+}
+
