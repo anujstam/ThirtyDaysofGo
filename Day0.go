de39895b08dc8c123f4d2852521787bb4a2f6c1e
Anuj Tambwekar <anujstam@gmail.com>
2019-02-24 10:54:44 +0530

    Create Day0.go

---
+++ Day0.go
@@ -0,0 +1,12 @@
+package main
+import (
+    "fmt"
+    "bufio"
+    "os"
+)
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    inp,_ := reader.ReadString('\n')
+    fmt.Println("Hello, World.")
+    fmt.Println(inp)
+}
