b0f515405ff27ff6add98616db8f94c634f8344c
Anuj Tambwekar <anujstam@gmail.com>
2019-02-24 11:01:10 +0530

    Create Day8.go

---
+++ Day8.go
@@ -0,0 +1,37 @@
+package main
+import (
+    "fmt"
+    "bufio"
+    "os"
+    "io"
+)
+
+
+func givenumber(input io.Reader) error {
+    var phoneNum int
+    fmt.Scan(&phoneNum)
+
+    phoneBook := make(map[string]string)
+    for i := 0; i < phoneNum; i++ {
+        var name, phone string
+        fmt.Scan(&name, &phone)
+        phoneBook[name] = phone
+    }
+
+    s := bufio.NewScanner(input)
+    for s.Scan() {
+        nameToPrint := s.Text()
+        if p, ok := phoneBook[nameToPrint]; ok {
+            fmt.Printf("%s=%s\n", nameToPrint, p)
+        } else {
+            fmt.Println("Not found")
+        }
+    }
+
+    return nil
+}
+
+func main() {
+    givenumber(os.Stdin)
+}
+
