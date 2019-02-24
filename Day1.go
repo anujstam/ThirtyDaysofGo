c4a8a482c1028bff48922f6264844a357c096581
Anuj Tambwekar <anujstam@gmail.com>
2019-02-24 10:55:29 +0530

    Create Day1.go

---
+++ Day1.go
@@ -0,0 +1,42 @@
+package main
+
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strconv"
+)
+
+func main() {
+  	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
+  
+    var i uint64 = 4
+    var d float64 = 4.0
+    var s string = "HackerRank "
+
+    scanner := bufio.NewScanner(os.Stdin)
+    // Declare second integer, double, and String variables.
+    var ip1 uint64
+    var ip2 float64
+    var ip3 string
+
+
+    // Read and save an integer, double, and String to your variables.
+    
+    fmt.Scan(&ip1)
+    fmt.Scan(&ip2)
+    _ = scanner
+    reader := bufio.NewReader(os.Stdin)
+    ip3,_ = reader.ReadString('\n')
+    
+    // Print the sum of both integer variables on a new line.
+    fmt.Println(i+ip1)
+    
+    // Print the sum of the double variables on a new line.
+    fmt.Printf("%.1f\n",d+ip2)
+    // Concatenate and print the String variables on a new line
+    fmt.Println(s+ip3)
+    // The 's' variable above should be printed first.
+
+}
