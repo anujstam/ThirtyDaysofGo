bcc521c7efa4f013cc24efa059e9bdcd2a13dca3
Anuj Tambwekar <anujstam@gmail.com>
2019-03-06 17:28:26 +0530

    Day 10 - Binary Numbers

--- Solutions/Day10/Day10.go
+++ Solutions/Day10/Day10.go
@@ -32,7 +32,6 @@ func maxones(n int32) int{
 
 func main() {
     reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
-
     nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
     checkError(err)
     n := int32(nTemp)
